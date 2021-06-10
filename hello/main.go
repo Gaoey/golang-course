16b831cad76483a6f5c824fafac747be39f65d73
Peeravas Reungsawas <peeravas.r@lmwn.com>
2021-06-10 10:45:05 +0700

    multiple function

--- hello/main.go
+++ hello/main.go
@@ -3,8 +3,13 @@ package main
 func main() {
 	result := squareArea(2)
 	println("result: ", result)
+	println(multiple(1, 2))
 }
 
 func squareArea(a int) int {
 	return a * a
+}
+
+func multiple(a, b int) (int, int) {
+	return b, a + b
 }
