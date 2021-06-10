b866c5b2a1e8b76ded81f8fab7f90bc352618c34
Peeravas Reungsawas <peeravas.r@lmwn.com>
2021-06-10 11:00:20 +0700

    isEven input as string

--- hello/main.go
+++ hello/main.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	result := squareArea(2)
 	println("result: ", result)
 	println(multiple(1, 2))
-	println(isEven(2))
+
+	v, err := isEven("aa")
+	if err != nil {
+		fmt.Printf("err %s", err.Error())
+	}
+
+	fmt.Printf("isEven result: %v", v)
+
 }
 
 func squareArea(a int) int {
@@ -15,6 +27,11 @@ func multiple(a, b int) (int, int) {
 	return b, a + b
 }
 
-func isEven(a int) bool {
-	return a%2 == 0
+func isEven(a string) (bool, error) {
+	b, err := strconv.Atoi(a)
+	if err != nil {
+		return false, err
+	}
+
+	return b%2 == 0, nil
 }
